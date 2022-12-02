45d7aa6527d3e556fe30af0e4fb9d87947c2e420
HN-Vignolles <hn.vignolles@gmail.com>
2022-12-02 11:38:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ from pathlib import Path
 samples_path = Path('./datasets')
 
 
-sound_events = ['Pentametric-fan-event-A',
-                'Pentametric-fan-event-B',
-                'Amulite-Vibration',
-                'Orth-Effect',
-                'Marzelvane-Sync','_test']
+sound_events = [
+    'Moss',
+    'Roy',
+    'Jen',
+    '_test'
+]
 sound_events.sort()
